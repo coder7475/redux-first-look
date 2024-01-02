0773ba62341ffafce1308623bc29b7342f448175
coder7475 <robiulhossain7475@gmail.com>
2024-01-02 11:27:04 +0600

    feat: write down all the steps for redux app in comment in index file

---
+++ index.jsx
@@ -0,0 +1,14 @@
+// create a counter app state management using redux
+// only for learning purpose
+
+/**
+ * TODO: Create redux app state management for a simple counter app
+ * * 1. Initial State Declaration
+ * * 2. Create pure functions for redux actions
+ * * 3. Create reducer function to handle logic
+ * * 4. Create Store to save states and subscribe to UI
+ * * 5. Subscribe to cosole
+ * * 6. Dispatch actions from store
+ * ? See the output
+ */
+
